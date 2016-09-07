7d4ef23af0f7920c930a30edc444295e7afcfdcc
Jimmy Lin <usmcjimmy@gmail.com>
2016-09-07 16:49:00 -0400

    added login modal with login options

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,14 @@
 $(function(){
   console.log("running script file")
+
   
+
+  // sign in / sign out Modal
+  $('#login_button').on('click', function(){
+    console.log("clicked")
+    $('.ui.small.modal.login').modal('show')
+
+  })
+
+
 })
