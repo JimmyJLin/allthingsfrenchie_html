30ca401d780173f92dabd9eaf2fb8ae3df036886
Jimmy Lin <usmcjimmy@gmail.com>
2016-09-07 20:35:41 -0400

    added user auth for login, signup and forget_password

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,26 @@
 $(function(){
   console.log("running script file")
 
-  
+
 
   // sign in / sign out Modal
   $('#login_button').on('click', function(){
-    console.log("clicked")
+    console.log("login_button clicked")
     $('.ui.small.modal.login').modal('show')
 
   })
 
+  $('#signup_button').on('click', function(){
+    console.log("signup_button clicked")
+    $('.ui.small.modal.signup').modal('show')
+
+  })
+
+
+$('#reset_password_button').on('click', function(){
+  console.log("signup_button clicked")
+  $('.ui.small.modal.reset_password').modal('show')
+
+})
 
 })
